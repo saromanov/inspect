c726d8ddc1253d3c475df0706cf04766ed609fe6
Sergey <xxsmotur@gmail.com>
2020-01-31 22:20:28 +0500

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
-	"strings"
-	"time"
 
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
 type Output struct {
-	Name          string `json:",omitempty"`
-	Tag           string `json:",omitempty"`
+	Name string `json:",omitempty"`
+	Tag  string `json:",omitempty"`
+}
+
+type inspectOptions struct {
+	global *globalOptions
+	image  *imageOptions
+	raw    bool
+	config bool
 }
 
 type Options struct {
-	
 }
 
 func command(global *globalOptions) cli.Command {
@@ -29,22 +30,36 @@ func command(global *globalOptions) cli.Command {
 		image:  imageOpts,
 	}
 	return cli.Command{
-		Name:  "inspect",
-		Usage: "Inspect image IMAGE-NAME",
-		Description: "Description"
-		ArgsUsage: "NAME",
-		Flags: append(append([]cli.Flag{
-		}, sharedFlags...), imageFlags...),
-		Action: commandAction(opts.run),
+		Name:        "inspect",
+		Usage:       "Inspect image IMAGE-NAME",
+		Description: "Description",
+		ArgsUsage:   "NAME",
+		Flags:       append(append([]cli.Flag{}, sharedFlags...), imageFlags...),
+		Action:      commandAction(opts.run),
 	}
 }
 
 func (opts *inspectOptions) run(args []string, stdout io.Writer) (retErr error) {
 	ctx, cancel := opts.global.commandTimeoutContext()
 	defer cancel()
+
+	if len(args) != 1 {
+		return errors.New("Exactly one argument expected")
+	}
+	imageName := args[0]
+
+	if err := reexecIfNecessaryForImages(imageName); err != nil {
+		return err
+	}
+
+	sys, err := opts.image.newSystemContext()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func main(){
+func main() {
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/saromanov/inspect
 
 go 1.13
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/urfave/cli v1.22.2
+)
